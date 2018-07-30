96a31f3ea7a463df210c9f60ebcdc74b45848abf
chris <butchchr@gmail.com>
2018-07-30 16:28:31 -0400

    Built lists of objects (movie) and can pull from them by genre.  need to refactor into methods and fix y/n always exit bug

--- gclab11movies/Movies.cs
+++ gclab11movies/Movies.cs
@@ -6,20 +6,39 @@ using System.Threading.Tasks;
 
 namespace gclab11movies
 {
-    class Movies
+    class Movie
     {
-        public string GetTitle { get; set; }
+        //public string GetTitle { get; set; }
 
-        public string GetCategory { get; set; }
+        //public string GetCategory { get; set; }
 
-        private string Title()
+        //private string Title()
+        //{
+        //    return "In theory this is a title";
+        //}
+
+        //private string Category()
+        //{
+        //    return "In theory this is a Cate";
+        //}
+
+        public Movie(string title, string genre)
         {
-
+            Title = title;
+            Genre = genre;
         }
 
-        private string Category()
-        {
+        private string Title;
+        private string Genre;
 
+        public string GetTitle()
+        {
+            return Title;
+        }
+
+        public string GetGenre()
+        {
+            return Genre;
         }
     }
 }
--- gclab11movies/Program.cs
+++ gclab11movies/Program.cs
@@ -15,9 +15,22 @@ namespace gclab11movies
         const string Scifi = "scifi";
 
         static void Main(string[] args)
-        {
-            List<string> movieType = new List<string> { Animated, Drama, Horror, Scifi };
-            //Logic logic = new Logic
+        {  
+            List<Movie> movieList = new List<Movie>();
+
+            movieList.Add(new Movie("Finding Nemo", "animated"));
+            movieList.Add(new Movie("Shrek", "animated"));
+            movieList.Add(new Movie("Frozen", "animated"));
+            movieList.Add(new Movie("Citizen Kane", "drama"));
+            movieList.Add(new Movie("Metropolis", "drama"));
+            movieList.Add(new Movie("The Godfather", "drama"));
+            movieList.Add(new Movie("Blair Witch Project", "horror"));
+            movieList.Add(new Movie("It", "horror"));
+            movieList.Add(new Movie("The Ring", "horror"));
+            movieList.Add(new Movie("The Matrix", "scifi"));
+            movieList.Add(new Movie("Galaxy Quest", "scifi"));
+            movieList.Add(new Movie("Star Trek II, The Wrath of Khan", "scifi"));
+            
             bool run = true;
             while (run)
             {
@@ -28,19 +41,43 @@ namespace gclab11movies
                 {
                     if (userChoice == Animated)
                     {
-                        //display animated
+                        foreach(Movie movie in movieList)
+                        {
+                            if(movie.GetGenre() == Animated)
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
                     }
                     else if (userChoice == Drama)
                     {
-                        //display drama
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == Drama)
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
                     }
                     else if (userChoice == Horror)
                     {
-                        //display horror
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == Horror)
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
                     }
                     else if (userChoice == Scifi)
                     {
-                        //display Scifi
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == Scifi)
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
                     }
                 }
                 else
